a45f9268fe97cde05f242f17883b5b6fd291c556
Tim Neumann <git@neumann-tim.de>
2024-11-12 20:24:45 +0100

    Remove users section as that was messing things up

--- users.nix
+++ users.nix
@@ -2,13 +2,4 @@
 
 {
   sops.secrets.my-password.neededForUsers = true;
-  users = {
-    users = {
-      example = {
-        isNormalUser = true;
-        uid = 1001;
-        hashedPasswordFile = config.sops.secrets.my-password.path;
-      };
-    };
-  };
 }
