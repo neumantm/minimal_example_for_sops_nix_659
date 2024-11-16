35827a77556090823fcfabeb658c6582fe5ef0b2
Tim Neumann <git@neumann-tim.de>
2024-11-16 19:11:02 +0100

    chore: Switch to first broken commit of sops-nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  inputs.sops-nix.url = "github:Mic92/sops-nix";
+  inputs.sops-nix.url = "github:Mic92/sops-nix/c9f6b151cc3578f6517a5dbd152f0e29cc6d875f";
   inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = inputs: {
